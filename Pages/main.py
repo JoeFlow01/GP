63dcddd9500b8101f43825c496b8c2ef1d2a0b9e
Youssef Assy <youf2001@outlook.com>
2024-06-24 03:35:04 +0300

    Last commit isa

--- Pages/main.py
+++ Pages/main.py
@@ -1,15 +1,172 @@
-import os
-print(os.getcwd())
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import cv2
 import numpy as np
 import os
+import matplotlib.pyplot as plt
+import time
 import mediapipe as mp
+import tensorflow as tf
+import pyttsx3  # <--- Added this import
+
+# In[2]:
+
+
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.regularizers import l2
+
+# In[3]:
+
+
+# Initialize the TTS engine
+engine = pyttsx3.init()  # <--- Added this line
+
+
+def speak(text):  # <--- Added this function
+    """Convert text to speech"""
+    engine.say(text)
+    engine.runAndWait()
+
+
+# In[4]:
+
+
+# Path for exported data, numpy arrays
+DATA_PATH = os.path.join('MP_Data_Final')
+
+# Actions that we try to detect
+actions = np.array(['hello', 'help', 'thanks', 'iloveyou', 'sorry', 'eat'])
+
+# Increased number of videos worth of data
+no_sequences = 30  # Adjusted to 60 for more training data
+
+# Videos are going to be 30 frames in length
+sequence_length = 30
+
+label_map = {label: num for num, label in enumerate(actions)}
+
+# In[5]:
+
+
+from sklearn.metrics import multilabel_confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+
+# In[6]:
+
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30, 1662)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+
+# In[7]:
+
+
+sequences, labels = [], []
+for action in actions:
+    for sequence in range(no_sequences):
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+
+X = np.array(sequences)
+y = to_categorical(labels).astype(int)
+
+# In[8]:
+
+
+label_map = {label: num for num, label in enumerate(actions)}
+np.array(sequences).shape
+X = np.array(sequences)
+y = to_categorical(labels).astype(int)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+y_test.shape
+
+# In[9]:
+
+
+model.load_weights('action#incr6.h5')
+
+# In[10]:
+
+
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+
+# In[11]:
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+
+# In[12]:
+
+
+yhat = model.predict(X_test)
+
+# In[13]:
+
+
+ytrue = np.argmax(y_test, axis=1).tolist()
+yhat = np.argmax(yhat, axis=1).tolist()
+
+# In[14]:
+
+
+multilabel_confusion_matrix(ytrue, yhat)
+
+# In[15]:
+
+
+accuracy_score(ytrue, yhat)
+
+# In[16]:
+
+
+from scipy import stats
+
+
+# In[17]:
+
+
+def generate_colors(num_colors):
+    np.random.seed(0)  # Seed for color consistency
+    return [tuple(np.random.randint(0, 256, 3).tolist()) for _ in range(num_colors)]
+
+
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        color = colors[num % len(colors)]  # Cycle through colors if less colors than actions
+        cv2.rectangle(output_frame, (0, 60 + num * 40), (int(prob * 100), 90 + num * 40), color, -1)
+        cv2.putText(output_frame, actions[num], (0, 85 + num * 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
+                    cv2.LINE_AA)
+
+    return output_frame
+
+
+# In[18]:
+
 
 # Initialize MediaPipe Holistic
 mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
+
+
+# In[19]:
 
 
 def mediapipe_detection(image, model):
@@ -19,6 +176,9 @@ def mediapipe_detection(image, model):
     image.flags.writeable = True  # Image writeable
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # Convert color RGB to BGR
     return image, results
+
+
+# In[20]:
 
 
 def draw_styled_landmarks(image, results):
@@ -55,6 +215,9 @@ def draw_styled_landmarks(image, results):
         )
 
 
+# In[21]:
+
+
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in
                      results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33 * 4)
@@ -66,6 +229,219 @@ def extract_keypoints(results):
                    results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(
         21 * 3)
     return np.concatenate([pose, face, lh, rh])
+
+
+# In[22]:
+
+
+# New detection variables
+sequence = []
+sentence = []
+predictions = []
+threshold = 0.95
+
+# Actions and Colors
+colors = generate_colors(len(actions))  # Generate dynamic colors
+
+cap = cv2.VideoCapture(0)
+# Set mediapipe model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        # Read feed
+        ret, frame = cap.read()
+
+        # Make detections
+        image, results = mediapipe_detection(frame, holistic)
+        print(results)
+
+        # Draw landmarks
+        draw_styled_landmarks(image, results)
+
+        # Prediction logic
+        keypoints = extract_keypoints(results)
+        sequence.append(keypoints)
+        sequence = sequence[-30:]
+
+        if len(sequence) == 30:
+            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            print(actions[np.argmax(res)])
+            predictions.append(np.argmax(res))
+
+            # Viz logic
+            if np.unique(predictions[-10:])[0] == np.argmax(res):
+                if res[np.argmax(res)] > threshold:
+                    if len(sentence) > 0:
+                        if actions[np.argmax(res)] != sentence[-1]:
+                            sentence.append(actions[np.argmax(res)])
+                            speak(actions[np.argmax(res)])  # <--- Added this line
+                    else:
+                        sentence.append(actions[np.argmax(res)])
+                        speak(actions[np.argmax(res)])  # <--- Added this line
+
+            if len(sentence) > 5:
+                sentence = sentence[-5:]
+
+            # Viz probabilities
+            image = prob_viz(res, actions, image, colors)
+
+        # Display Text
+        cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
+        cv2.putText(image, ' '.join(sentence), (3, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # Show to screen
+        cv2.imshow('OpenCV Feed', image)
+
+        # Break gracefully
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import time
+import mediapipe as mp
+import tensorflow as tf
+import pyttsx3  # <--- Added this import
+
+# In[2]:
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.regularizers import l2
+
+# In[3]:
+
+
+# Initialize the TTS engine
+engine = pyttsx3.init()  # <--- Added this line
+
+
+def speak(text):  # <--- Added this function
+    """Convert text to speech"""
+    engine.say(text)
+    engine.runAndWait()
+
+
+# In[4]:
+
+
+# Path for exported data, numpy arrays
+DATA_PATH = os.path.join('MP_Data_Final')
+
+# Actions that we try to detect
+actions = np.array(['hello', 'help', 'thanks', 'iloveyou', 'sorry', 'eat'])
+
+# Increased number of videos worth of data
+no_sequences = 30  # Adjusted to 60 for more training data
+
+# Videos are going to be 30 frames in length
+sequence_length = 30
+
+label_map = {label: num for num, label in enumerate(actions)}
+
+# In[5]:
+
+
+from sklearn.metrics import multilabel_confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+
+# In[6]:
+
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30, 1662)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+
+# In[7]:
+
+
+sequences, labels = [], []
+for action in actions:
+    for sequence in range(no_sequences):
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+
+X = np.array(sequences)
+y = to_categorical(labels).astype(int)
+
+# In[8]:
+
+
+label_map = {label: num for num, label in enumerate(actions)}
+np.array(sequences).shape
+X = np.array(sequences)
+y = to_categorical(labels).astype(int)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+y_test.shape
+
+# In[9]:
+
+
+model.load_weights('action#incr6.h5')
+
+# In[10]:
+
+
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+
+# In[11]:
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+
+# In[12]:
+
+
+yhat = model.predict(X_test)
+
+# In[13]:
+
+
+ytrue = np.argmax(y_test, axis=1).tolist()
+yhat = np.argmax(yhat, axis=1).tolist()
+
+# In[14]:
+
+
+multilabel_confusion_matrix(ytrue, yhat)
+
+# In[15]:
+
+
+accuracy_score(ytrue, yhat)
+
+# In[16]:
+
+
+from scipy import stats
+
+
+# In[17]:
 
 
 def generate_colors(num_colors):
@@ -84,19 +460,81 @@ def prob_viz(res, actions, input_frame, colors):
     return output_frame
 
 
-# Define actions and colors
-actions = np.array(['hello', 'thanks', 'iloveyou' ,'help','eat', 'yes', 'sorry', 'no'])  # Replace with your actual actions
-colors = generate_colors(len(actions))
+# In[18]:
 
-# Load the trained model
-model = Sequential()
-model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30, 1662)))
-model.add(LSTM(128, return_sequences=True, activation='relu'))
-model.add(LSTM(64, return_sequences=False, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(actions.shape[0], activation='softmax'))
-model.load_weights('action60.h5')
+
+# Initialize MediaPipe Holistic
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+
+
+# In[19]:
+
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert color BGR to RGB
+    image.flags.writeable = False  # Image not writeable
+    results = model.process(image)  # Process to get landmarks
+    image.flags.writeable = True  # Image writeable
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # Convert color RGB to BGR
+    return image, results
+
+
+# In[20]:
+
+
+def draw_styled_landmarks(image, results):
+    # Draw face landmarks
+    if results.face_landmarks:
+        mp_drawing.draw_landmarks(
+            image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+            mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+            mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+        )
+
+    # Draw pose landmarks
+    if results.pose_landmarks:
+        mp_drawing.draw_landmarks(
+            image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+            mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+            mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+        )
+
+    # Draw left hand landmarks
+    if results.left_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+            mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+            mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+        )
+
+    # Draw right hand landmarks
+    if results.right_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+            mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+            mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+        )
+
+
+# In[21]:
+
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                     results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33 * 4)
+    face = np.array([[res.x, res.y, res.z] for res in
+                     results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468 * 3)
+    lh = np.array([[res.x, res.y, res.z] for res in
+                   results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21 * 3)
+    rh = np.array([[res.x, res.y, res.z] for res in
+                   results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(
+        21 * 3)
+    return np.concatenate([pose, face, lh, rh])
+
+
+# In[22]:
+
 
 # New detection variables
 sequence = []
@@ -104,8 +542,11 @@ sentence = []
 predictions = []
 threshold = 0.95
 
+# Actions and Colors
+colors = generate_colors(len(actions))  # Generate dynamic colors
+
 cap = cv2.VideoCapture(0)
-# Set MediaPipe model
+# Set mediapipe model
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     while cap.isOpened():
         # Read feed
@@ -113,6 +554,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 
         # Make detections
         image, results = mediapipe_detection(frame, holistic)
+        print(results)
 
         # Draw landmarks
         draw_styled_landmarks(image, results)
@@ -124,6 +566,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 
         if len(sequence) == 30:
             res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            print(actions[np.argmax(res)])
             predictions.append(np.argmax(res))
 
             # Viz logic
@@ -132,8 +575,10 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                     if len(sentence) > 0:
                         if actions[np.argmax(res)] != sentence[-1]:
                             sentence.append(actions[np.argmax(res)])
+                            speak(actions[np.argmax(res)])  # <--- Added this line
                     else:
                         sentence.append(actions[np.argmax(res)])
+                        speak(actions[np.argmax(res)])  # <--- Added this line
 
             if len(sentence) > 5:
                 sentence = sentence[-5:]
@@ -151,6 +596,9 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         # Break gracefully
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
-
     cap.release()
     cv2.destroyAllWindows()
+
+
+
+
